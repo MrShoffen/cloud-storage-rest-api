6b6799f92b0f06d402cb899f8605669606b99eeb
andrey_vasiliev <mrshoffen@gmail.com>
2025-01-30 22:09:55 +0300

    chore: add liquibase and migration schemes.

---
+++ src/main/resources/db/changelog/user-table.sql
@@ -0,0 +1,13 @@
+--liquibase formatted sql
+
+--changeset mrshoffen:1
+CREATE SCHEMA IF NOT EXISTS cloudStorage;
+
+--changeset mrshoffen:2
+CREATE TABLE IF NOT EXISTS cloudStorage.users
+(
+    id         BIGSERIAL PRIMARY KEY,
+    username   VARCHAR(255) NOT NULL UNIQUE,
+    password   VARCHAR(255) NOT NULL,
+    avatar_url VARCHAR(256)
+);
